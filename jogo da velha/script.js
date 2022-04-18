5e03c94337bedbc3cc4ebd3838a4be5440251d6b
menezescosta <mc611641@gmail.com>
2022-04-18 20:45:38 +0100

    fazendo todas as funcionalidades

--- jogo da velha/script.js
+++ jogo da velha/script.js
@@ -15,6 +15,7 @@ let playing=false;
 reset();
 //events
 document.querySelector('.reset').addEventListener('click',reset);
+
 document.querySelectorAll('.item').forEach((item)=>{
     item.addEventListener('click',itemClicked);
 });
@@ -22,7 +23,7 @@ document.querySelectorAll('.item').forEach((item)=>{
 function itemClicked(event) {
     let item=event.target.getAttribute('data-item');
 
-    if(square[item]===''){
+    if(playing && square[item]===''){
         square[item]=playerTime;
         renderSquare();
         togglePlayer();
@@ -56,6 +57,7 @@ function renderSquare() {
         let item=document.querySelector(`div[data-item=${i}]`);
         item.innerHTML=square[i];
     }
+    checkGame();
 }
 function renderInfo() {
     document.querySelector('.vez').innerHTML=playerTime;
@@ -64,4 +66,48 @@ function renderInfo() {
 function togglePlayer() {
     playerTime=(playerTime==='x')?'o':'x';
     renderInfo();
+}
+function checkGame() {
+    if(checkWinnerFor('x')){
+        warnning=`o "x" vencceu`;
+        playing=false;
+    }else  
+    if(checkWinnerFor('o')){
+        warnning=`o "o" vencceu`;
+        playing=false;
+    }else
+    if(isFull()){
+        warnning=`Deu enpate`;
+        playing=false;
+    }
+
+}
+function checkWinnerFor(player) {
+    let pos=['a1,a2,a3',
+            'b1,b2,b3',
+            'c1,c2,c3',
+            'a1,b1,c1',
+            'a2,b2,c2',
+            'a3,b3,c3',
+            'a1,b2,c3',
+            'a3,b2,c1'];
+
+            for(let w in pos){
+                let pArray=pos[w].split(',');
+
+                let hasWon=pArray.every(option=>square[option]===player);
+
+                if(hasWon){
+                    return true;
+                }
+            }
+            return false;
+}
+function isFull() {
+    for(let i in square){
+        if(square[i]===''){
+            return false;
+        }       
+    }
+    return true;
 }
